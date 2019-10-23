7fc9af477b074bc5f05dbd88a42896d022a229f8
Rukai <rubickent@gmail.com>
2019-10-24 00:28:51 +1100

    Manually implement cubicspline interpolation

--- canon_collision/src/wgpu/animation.rs
+++ canon_collision/src/wgpu/animation.rs
@@ -2,7 +2,9 @@ use crate::wgpu::model3d::{Animation, Joint, ChannelOutputs, Channel};
 
 use cgmath::{Matrix4, VectorSpace};
 use gltf::animation::Interpolation;
-use splines::{Interpolation as SplineInterpolation, Key, Spline};
+
+// Cubicspline interpolation implemented as per:
+// https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/README.md#appendix-c-spline-interpolation
 
 pub fn set_animated_joints(animation: &Animation, frame: f32, root_joint: &mut Joint, parent_transform: Matrix4<f32>) {
     let mut translation = root_joint.translation.clone();
@@ -24,17 +26,24 @@ pub fn set_animated_joints(animation: &Animation, frame: f32, root_joint: &mut J
                     translation = translations[output_index];
                 }
                 (ChannelOutputs::Translations (translations), Interpolation::CubicSpline) => {
-                    let seconds = frame / 60.0; // 60fps
-                    let mut points = vec!();
-                    for (input, outputs) in channel.inputs.iter().zip(translations.chunks(3)) {
-                        points.push(Key::new(*input, outputs[1], SplineInterpolation::StrokeBezier(outputs[0], outputs[2])));
-                    }
-                    let spline = Spline::from_vec(points);
-                    if let Some(result) = spline.clamped_sample(seconds) {
-                        translation = result;
-                    }
-                    else {
-                        error!("Failed to interpolate translation spline");
+                    translation = match index_cubicspline(channel, frame) {
+                        CubicSplineIndex::Clamped { index } => {
+                            translations[index * 3 + 1]
+                        }
+                        CubicSplineIndex::Interpolate { index_pre, index_next, t, range } => {
+                            let p0 =         translations[index_pre  * 3 + 1]; // previous spline vertex
+                            let p1 =         translations[index_next * 3 + 1]; // next spline vertex
+                            let m0 = range * translations[index_pre  * 3 + 2]; // previous output tangent
+                            let m1 = range * translations[index_next * 3 + 0]; // next input tangent
+
+                            let tpow2 = t * t;
+                            let tpow3 = tpow2 * t;
+
+                            (2.0 * tpow3 - 3.0 * tpow2 + 1.0) * p0 +
+                            (tpow3 - 2.0 * tpow2 + t) * m0 +
+                            (-2.0 * tpow3 + 3.0 * tpow2) * p1 +
+                            (tpow3 - tpow2) * m1
+                        }
                     }
                 }
                 (ChannelOutputs::Rotations (rotations), Interpolation::Linear) => {
@@ -48,17 +57,24 @@ pub fn set_animated_joints(animation: &Animation, frame: f32, root_joint: &mut J
                     rotation = rotations[output_index].into();
                 }
                 (ChannelOutputs::Rotations (rotations), Interpolation::CubicSpline) => {
-                    let seconds = frame / 60.0; // 60fps
-                    let mut points = vec!();
-                    for (input, outputs) in channel.inputs.iter().zip(rotations.chunks(3)) {
-                        points.push(Key::new(*input, outputs[1], SplineInterpolation::StrokeBezier(outputs[0], outputs[2])));
-                    }
-                    let spline = Spline::from_vec(points);
-                    if let Some(result) = spline.clamped_sample(seconds) {
-                        rotation = result;
-                    }
-                    else {
-                        error!("Failed to interpolate rotation spline");
+                    rotation = match index_cubicspline(channel, frame) {
+                        CubicSplineIndex::Clamped { index } => {
+                            rotations[index * 3 + 1]
+                        }
+                        CubicSplineIndex::Interpolate { index_pre, index_next, t, range } => {
+                            let p0 =         rotations[index_pre  * 3 + 1]; // previous spline vertex
+                            let p1 =         rotations[index_next * 3 + 1]; // next spline vertex
+                            let m0 = range * rotations[index_pre  * 3 + 2]; // previous output tangent
+                            let m1 = range * rotations[index_next * 3 + 0]; // next input tangent
+
+                            let tpow2 = t * t;
+                            let tpow3 = tpow2 * t;
+
+                            (2.0 * tpow3 - 3.0 * tpow2 + 1.0) * p0 +
+                            (tpow3 - 2.0 * tpow2 + t) * m0 +
+                            (-2.0 * tpow3 + 3.0 * tpow2) * p1 +
+                            (tpow3 - tpow2) * m1
+                        }
                     }
                 }
                 (ChannelOutputs::Scales (scales), Interpolation::Linear) => {
@@ -72,19 +88,26 @@ pub fn set_animated_joints(animation: &Animation, frame: f32, root_joint: &mut J
                     scale = scales[output_index];
                 }
                 (ChannelOutputs::Scales (scales), Interpolation::CubicSpline) => {
-                    let seconds = frame / 60.0; // 60fps
-                    let mut points = vec!();
-                    for (input, outputs) in channel.inputs.iter().zip(scales.chunks(3)) {
-                        points.push(Key::new(*input, outputs[1], SplineInterpolation::StrokeBezier(outputs[0], outputs[2])));
-                        println!("spline vertex: {:?}, input tangent: {:?}, output tangent: {:?}", outputs[1], outputs[0], outputs[2]);
-                    }
-                    let spline = Spline::from_vec(points);
-                    if let Some(result) = spline.clamped_sample(seconds) {
-                        scale = result;
-                    }
-                    else {
-                        error!("Failed to interpolate scale spline");
-                    }
+                    scale = match index_cubicspline(channel, frame) {
+                        CubicSplineIndex::Clamped { index } => {
+                            scales[index * 3 + 1]
+                        }
+                        CubicSplineIndex::Interpolate { index_pre, index_next, t, range } => {
+                            let p0 =         scales[index_pre  * 3 + 1]; // previous spline vertex
+                            let p1 =         scales[index_next * 3 + 1]; // next spline vertex
+                            let m0 = range * scales[index_pre  * 3 + 2]; // previous output tangent
+                            let m1 = range * scales[index_next * 3 + 0]; // next input tangent
+
+                            let tpow2 = t * t;
+                            let tpow3 = tpow2 * t;
+
+                            (2.0 * tpow3 - 3.0 * tpow2 + 1.0) * p0 +
+                            (tpow3 - 2.0 * tpow2 + t) * m0 +
+                            (-2.0 * tpow3 + 3.0 * tpow2) * p1 +
+                            (tpow3 - tpow2) * m1
+                        }
+                    };
+                    println!("scale: {:?}", scale);
                 }
                 (_, Interpolation::CatmullRomSpline) => unimplemented!("This will be deleted in next gltf version"),
             }
@@ -143,4 +166,36 @@ fn index_linear(channel: &Channel, frame: f32) -> (usize, usize, f32) {
     // seconds must be larger than highest input, so return the last index
     let last = channel.inputs.len() - 1;
     (last, last, 0.0)
+}
+
+enum CubicSplineIndex {
+    Clamped { index: usize },
+    Interpolate { index_pre: usize, index_next: usize, t: f32, range: f32 }
+}
+
+fn index_cubicspline(channel: &Channel, frame: f32) -> CubicSplineIndex {
+    let seconds = frame / 60.0; // 60fps
+
+    if seconds < *channel.inputs.first().unwrap() || channel.inputs.len() < 2 {
+        return CubicSplineIndex::Clamped { index: 0 }
+    }
+
+    for (i, window) in channel.inputs.windows(2).enumerate() {
+        let input_pre  = window[0];
+        let input_next = window[1];
+        if seconds >= input_pre && seconds < input_next {
+            let range = input_next - input_pre;
+            let t = (seconds - input_pre) / range;
+            return CubicSplineIndex::Interpolate {
+                index_pre: i,
+                index_next: i + 1,
+                t,
+                range
+            };
+        }
+    }
+
+    // seconds must be larger than highest input, so return the last index
+    let index = channel.inputs.len() - 1;
+    CubicSplineIndex::Clamped { index }
 }
