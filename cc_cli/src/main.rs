3f73189766c65c0efaafbe8f43e0e7362fafe5ce
Rukai <rubickent@gmail.com>
2020-07-07 23:16:02 +1000

    cc_cli outputs an exit code on error

--- cc_cli/src/main.rs
+++ cc_cli/src/main.rs
@@ -4,20 +4,28 @@ use std::io::Write;
 use std::net::TcpStream;
 
 fn main() {
+    std::process::exit(main_main());
+}
+
+fn main_main() -> i32 {
     let mut args = env::args();
     args.next();
     let out_vec: Vec<String> = args.collect();
     let out: String = format!("C{}", out_vec.join(" "));
 
-    let mut stream = match TcpStream::connect("127.0.0.1:1613") {
-        Ok(stream)  => { stream }
-        Err(e)      => { println!("Could not connect to PF Sandbox host: {}", e); return; }
-    };
+    match TcpStream::connect("127.0.0.1:1613") {
+        Ok(mut stream) => {
+            stream.write(out.as_bytes()).unwrap();
 
-    stream.write(out.as_bytes()).unwrap();
-
-    let mut result = String::new();
-    if let Ok(_) = stream.read_to_string(&mut result) {
-        println!("{}", result);
+            let mut result = String::new();
+            if let Ok(_) = stream.read_to_string(&mut result) {
+                println!("{}", result);
+            }
+            0
+        }
+        Err(e) => {
+            println!("Could not connect to PF Sandbox host: {}", e);
+            1
+        }
     }
 }
